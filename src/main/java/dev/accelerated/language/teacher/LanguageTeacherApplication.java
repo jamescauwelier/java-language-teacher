0927011bd2c2a4a3100946f8f5198d1180cb7095
James Cauwelier <james@accelerated.dev>
2025-01-12 15:26:47 -0800

    Creates a main application

---
+++ src/main/java/dev/accelerated/language/teacher/LanguageTeacherApplication.java
@@ -0,0 +1,11 @@
+package dev.accelerated.language.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageTeacherApplication {
+    public static void main(String... args) {
+        SpringApplication.run(LanguageTeacherApplication.class, args);
+    }
+}
