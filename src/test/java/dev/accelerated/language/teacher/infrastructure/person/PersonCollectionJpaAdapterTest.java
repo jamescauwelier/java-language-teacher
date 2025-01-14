50a5dfe1e162d2db759b77378ba4fa374044ae49
James Cauwelier <james@accelerated.dev>
2025-01-14 15:12:00 -0800

    Tests the JPA adapter for person collections

--- src/test/java/dev/accelerated/language/teacher/infrastructure/person/PersonCollectionJpaAdapterTest.java
+++ src/test/java/dev/accelerated/language/teacher/infrastructure/person/PersonCollectionJpaAdapterTest.java
@@ -1,21 +1,52 @@
 package dev.accelerated.language.teacher.infrastructure.person;
 
+import dev.accelerated.language.teacher.domain.person.Person;
+import dev.accelerated.language.teacher.domain.uuid.UUIDGeneratorPort;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class PersonCollectionJpaAdapterTest {
     @Autowired
-    PersonCollectionJpaAdapter adapter;
+    UUIDGeneratorPort uuid;
+
+    @Autowired
+    PersonCollectionJpaAdapter collection;
 
     @Test
     void aPersonCanBeAddedToACollection() {
-        // todo
+        UUID id = uuid.generate();
+        String firstName = "John";
+        String lastName = "Bon Jovi";
+        var person = collection.add(new Person(id, firstName, lastName));
+
+        assertEquals(id, person.getId());
+        assertEquals(firstName, person.getFirstName());
+        assertEquals(lastName, person.getLastName());
+    }
+
+    @Test
+    void aNonExistingPersonCannotBeFound() {
+        UUID id = uuid.generate();
+        assertEquals(Optional.empty(), collection.get(id));
     }
 
     @Test
     void aPersonCanBeFoundAfterAdding() {
-        //todo
+        UUID id = uuid.generate();
+        String firstName = "John";
+        String lastName = "Bon Jovi";
+        collection.add(new Person(id, firstName, lastName));
+        var person = collection.get(id).get();
+
+        assertEquals(id, person.getId());
+        assertEquals(firstName, person.getFirstName());
+        assertEquals(lastName, person.getLastName());
     }
 }
